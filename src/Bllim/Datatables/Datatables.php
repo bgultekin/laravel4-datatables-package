3caa9b96c315c860f0046e7aba36a269f73ca84b
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2014-09-27 13:21:58 -0300

    Remove orderBy from select when counting

--- src/Bllim/Datatables/Datatables.php
+++ src/Bllim/Datatables/Datatables.php
@@ -745,6 +745,9 @@ class Datatables
             }
         }
 
+	    // Clear the orders, since they are not relevant for count
+	    $myQuery->orders = null;
+
         $this->$count = DB::connection($connection)
         ->table(DB::raw('('.$myQuery->toSql().') AS count_row_table'))
         ->setBindings($myQuery->getBindings())->count();
